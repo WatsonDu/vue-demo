a00aedbab46a0ff16a72c2f9cb3d821d405054b1
duxuanrain <598991476qq.com>
2017-10-08 22:32:22 +0800

    添加饿了么UI

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Topbar class="topbar"/>
+    <main>
+    <Editor class="editor"/>
+    <Preview class="preview"/>
+    </main>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor'
+import Topbar from './components/Topbar'
+import Preview from './components/Preview'
+export default {
+  components: {
+  Topbar,Editor,Preview,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.topbar{
+
+}
+main{
+display:flex;
+}
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,16 @@
+<template>
+<div id = "topbar">
+VueResumer
+<div class="actions">
+<button>注册</button>
+<button>登录</button>
+</div>
+</div>
+</template>
+<style>
+#topbar{
+display:flex;
+justify-content:space-between;
+padding:20px;
+}
+</style>
